11e98e8f5c96890d7f2d0b6e4c00cd8a5519dd9c
lennardDenby <lennardDenby>
2024-10-17 00:22:59 +0200

    reqirements, bot layout, system instruction change

--- src/LLM.py
+++ src/LLM.py
@@ -1,15 +1,16 @@
 from secret import GEMINI_API_KEY
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
-import prompts as prompts
+import prompts
 
 class LLM:
     def __init__(self):
+        
         genai.configure(api_key=GEMINI_API_KEY)
-        self.model = genai.GenerativeModel(
-            model_name="gemini-1.5-flash",
-            system_instruction=prompts.default
-        )
+        for e in genai.list_models():
+            print(e)
+        self.change_system_instruction("")
+        self.start_image_generator()
     
     def promptLLM(self, prompt: str, author: str) -> str:
         response = self.model.generate_content(
@@ -18,7 +19,14 @@ class LLM:
                 HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
                 HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
                 HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE
             }
         )
         return response.text
+    
+    def change_system_instruction(self, instruction: str) -> None:
+        print(instruction)
+        self.model = genai.GenerativeModel(
+            model_name="gemini-1.5-flash",
+            system_instruction=prompts.default + " " + instruction
+        )
--- src/main.py
+++ src/main.py
@@ -3,27 +3,38 @@ from secret import DISCORD_BOT_API_KEY
 import discord
 from discord.ext import commands
 
-class Client(commands.Bot):
-    async def on_ready(self):
-        print(f"Logged on as: {self.user}")
+class Client(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.llm = LLM()
         
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-        await self.process_commands(message)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"Logged on as: {self.bot.user}")
+    
+    @commands.command(name="ask")
+    async def ask(self, ctx, *, message):
+        await ctx.send(self.llm.promptLLM(message, ctx.author.display_name))
+    
+    @commands.command(name="instruct")
+    async def change_system_instuction(self, ctx, *, message):
+        self.llm.change_system_instruction(message)
+        await ctx.send("Ok, updating instructions.")
+    
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = Client(intents=intents, command_prefix="!")
-llm = LLM()
-
-@client.command(name="ask")
-async def ask(ctx, *, message):
-    await ctx.send(llm.promptLLM(message, ctx.author.display_name))
+class BotSetup(commands.Bot):
+    def __init__(self, intents, command_prefix):
+        super().__init__(intents=intents, command_prefix=command_prefix)
+        
+    async def setup_hook(self):
+        await self.add_cog(Client(self))
 
 def main():
-    client.run(DISCORD_BOT_API_KEY)
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot = BotSetup(intents=intents, command_prefix="!")
+    bot.run(DISCORD_BOT_API_KEY)
 
 if __name__ == '__main__':
     main()
